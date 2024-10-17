ee46b826e7082b87bd6ef9d8b502763e01a34b77
Gabriel Sena <gabrielsena8585@gmail.com>
2024-10-16 21:24:55 -0300

    Add files via upload

---
+++ ADD/Compra.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADD
+{
+    public class Compra
+    {
+        public int id {  get; set; }
+        [DisplayName("ID do Produto")] public int idProduto { get; set; }
+        [DisplayName("ID do Fornecedor")] public int idFornecedor { get; set; }
+        [DisplayName("Quantidade Comprada")] public int quantidade { get; set; }
+        [DisplayName("Porcentagem de Desconto")] public float desconto { get; set; }
+    }
+}
